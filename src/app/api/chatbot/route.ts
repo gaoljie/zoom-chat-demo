e6bbb6b411233277d86b9b8b8b2e5787359c1c27
Vishnu Gillella <vishnu.gillella@zoom.us>
2024-04-25 11:05:05 -0400

    Added createreminder chat bot command logic

--- src/app/api/chatbot/route.ts
+++ src/app/api/chatbot/route.ts
@@ -1,7 +1,8 @@
 import {getAccessToken} from "@/utils/getAccessToken";
 import {getFromAIService} from "@/utils/aiServiceClient";
-import {getReminderByUserId} from "../../db/databaseService";
+import {getReminderByUserId, saveReminder} from "../../db/databaseService";
 import {ReminderType} from "../../../types/reminderType";
+import {uuid} from "../../../utils/apiUtils";
 
 const zoomApiHost = process.env.ZOOM_API_HOST;
 
@@ -9,9 +10,8 @@ export async function POST(request: Request) {
     const botRequest = await request.json();
     const {
         event,
-        payload: {robotJid, toJid, accountId, userJid, cmd},
+        payload: {robotJid, toJid, accountId, userJid, cmd, messageId , fieldEditItem, actionItem},
     } = botRequest;
-
 
     console.log(JSON.stringify(botRequest));
     console.log(`cmd = ${cmd}`);
@@ -19,27 +19,44 @@ export async function POST(request: Request) {
     if (event === "bot_notification") {
         const command = getCommand(cmd);
         if (command === "create") {
-            await createReminder(botRequest);
+            await createReminderPendingConfirm(botRequest);
         } else if (command === "summary") {
             await summarizeReminders(botRequest);
         } else if (command === "list") {
             listReminders(botRequest);
         } else {
-            defaultCommand(botRequest);
+            defaultCommand(botRequest, "Command not found!", "Please try again", null);
         }
-        sendChatBotMsg(botRequest, "test");
         console.log(`successfully processed command = ${command}`);
+    } else if ( (event.startsWith("interactive") || event.startsWith("chat_message") || event === "interactive_message_editable" ) ||
+              (event === "interactive_message_fields_editable" ) ||
+              (event === "interactive_message_actions" ) ) {
+        console.log(`inside interactive messages, event = ${event}`);
+       if (actionItem) {
+           const actionText = botRequest.payload.original.head.text;
+           console.log(`actionText = ${actionText}`);
+           if (actionItem.action === "command" && actionItem.value === 'cancel') {
+               // ignore and delete the message.
+           } else if (actionItem.action === "command" && actionItem.value === 'approve') {
+                //save reminder to DB;
+              await createReminderAfterConfirm(botRequest);
+           }
+       } else {
+          await updateReminderFormFields(botRequest);
+       }
     }
 
     return Response.json({},{ status: 200 });
 }
 
 function getCommand(cmd: string) {
-    const command = cmd.split(" ")[0];
+    let regEx = /[\s,;:]+/;
+    const command = cmd.split(regEx)[0];
     return command;
 }
 
 async function sendChatBotMsg(botRequest, content: string) {
+    console.log(`sending chatBotMsg contentStr = ${content}`);
     const data = await (
         await fetch(`${zoomApiHost}/v2/im/chat/messages`, {
             method: "POST",
@@ -53,26 +70,35 @@ async function sendChatBotMsg(botRequest, content: string) {
     console.log(data);
 }
 
-async function createReminder(botRequest: any) {
+async function createReminderPendingConfirm(botRequest: any) {
     // parse cmd str and send notification.
     const {payload: {robotJid, toJid, accountId, userJid, cmd, userId}} = botRequest;
-    let aiRequest = `response Json example :{ "name": "Reminder for exercise", "startDate": ${new Date()} }.
+    let aiRequest = `response Json example :{ "name": "Reminder for exercise", "startDate": ${new Date().toLocaleString()} }.
                    If Month and year not specified in date, calculate dates after this start date :'2024-04-24 14:30:12'.
                    If the date is not available please use today date. 
                    If the month and year is not available in the date use today month and year. 
                    Based on the above instructions process below instructions.
-                   'Create reminder for my exercise today at 5 PM EST' based on this sentence extract the name of the event. 
-                   Response should be only event name,start date and end date  format  with 12 hour clock format (yyyy-MM-dd hh:mm:ss a) and timezone as IANA format. 
+                   '${cmd}' based on this sentence extract the name of the event. 
+                   Response should be only event name,start date format  with 24 hour clock format (yyyy-MM-dd hh:mm:ss) and timezone as IANA format. 
                    Populate the data in this json format {name: '', startDate: '', timeZone: ''} and return only the json.`;
-
+    console.log(`aiRequest = ${aiRequest}`);
     let aiResponse = await getFromAIService(aiRequest);
     let aiRespObj = JSON.parse(aiResponse);
-    console.log(`aiResponse = ${aiResponse}`);
+   // console.log(`aiResponse = ${aiResponse}`);
     console.log(`aiRespObj = ${aiRespObj}`);
     console.log(`aiRespObj.name = ${aiRespObj['name']}`);
     console.log(`aiRespObj.startDate = ${aiRespObj['startDate']}`);
     console.log(`aiRespObj.timeZone = ${aiRespObj['timeZone']}`);
-    //saveReminder
+
+   let aiRespStartDate = new Date(aiRespObj['startDate']);
+   const initialDate = aiRespStartDate.getFullYear().toString().padStart(4, '0') + "/" + (aiRespStartDate.getMonth()+1).toString().padStart(2, '0')+ "/" + aiRespStartDate.getDate().toString().padStart(2, '0');
+   const initialTime = aiRespStartDate.getHours().toString().padStart(2, '0') + ":" + aiRespStartDate.getMinutes().toString().padStart(2, '0');
+
+   let reminderObj = {"title" :aiRespObj.name, "dueDate": aiRespObj.startDate, "date": initialDate, "time": initialTime};
+
+   const contentStr = getContentStrForCreateReminderConfirmation(botRequest, reminderObj);
+   //send notification.
+    sendChatBotMsg(botRequest, contentStr);
 }
 
 async function listReminders(botRequest: any) {
@@ -90,13 +116,10 @@ async function listReminders(botRequest: any) {
         content: {
             settings: {
                 default_sidebar_color: "#0E72ED",
-                is_split_sidebar: false,
+                is_split_sidebar: true,
             },
             head: {
-                text: "List of Reminders",
-                sub_head: {
-                    text: "I am a sub head text",
-                },
+                text: "List of Reminders"
             },
             body: [
                 {
@@ -104,7 +127,7 @@ async function listReminders(botRequest: any) {
                     sections: result.map((reminder: ReminderType) => {
                         return {
                             type: "message",
-                            text: reminder.title,
+                            text: reminder.title + " => " + new Date(Date.parse(reminder.dueDate)).toLocaleString(),
                             style: {
                                 bold: false,
                             }
@@ -125,7 +148,262 @@ await function summarizeReminders(botRequest: any) {
     // parse cmd str and send notification.
 }
 
-function defaultCommand(botRequest: any) {
+function defaultCommand(botRequest: any , headText: string, subHeadText: string, content: any) {
+    // parse cmd str and send notification.
+    const {payload: {robotJid, toJid, accountId, userJid, cmd, userId}} = botRequest;
+    const contentStr = JSON.stringify({
+        robot_jid: robotJid,
+        to_jid: toJid,
+        account_id: accountId,
+        user_jid: userJid,
+        content: {
+            settings: {
+                default_sidebar_color: "#0E72ED",
+                is_split_sidebar: false,
+            },
+            head: {
+                text: headText,
+                sub_head: {
+                    text: subHeadText,
+                },
+            },
+            content: content
+        },
+    })
+    sendChatBotMsg(botRequest, contentStr);
+}
+
+async function createReminderAfterConfirm(botRequest: any) {
+    const {event, payload: {robotJid, toJid, accountId,userId, messageId , fieldEditItem, actionItem } } = botRequest;
+    const botReqPayloadObj  = botRequest.payload.original;
+    const msgId =  messageId;
+    let reminderObj = getReminderObjFromContentJson(botReqPayloadObj);
+    reminderObj.reminderId = uuid();
+    reminderObj.userId = userId;
+
+    console.log(`reminderObj is ${JSON.stringify(reminderObj)}`);
+
+    let result = await saveReminder(reminderObj);
+    if (result) {
+        defaultCommand(botRequest, "Success", "Successfully created reminder", null);
+        //finally delete the message.
+        deleteChatBotImMsg(msgId);
+    }
+}
+
+function  getReminderObjFromContentJson(botReqPayloadObj: any) : any {
+    let reminderObj: Partial<ReminderType> = {};
+    const sectionsArr = botReqPayloadObj.body.filter( e => e.type === 'section')[0].sections;
+    console.log(`sectionsArr = ${JSON.stringify(sectionsArr)}`);
+    const fieldsArr = sectionsArr.filter( e => e.type === 'fields')[0].items;
+    const datePickObj = sectionsArr.filter( e => e.type === 'datepicker').filter(e => e.action_id === 'datepicker123')[0];
+    const timePickObj = sectionsArr.filter( e => e.type === 'timepicker').filter(e => e.action_id === 'timepicker123')[0];
+    // console.log(`fieldsArr = ${JSON.stringify(fieldsArr)}`);
+    // console.log(`datePickObj = ${JSON.stringify(datePickObj)}`);
+    // console.log(`timePickObj = ${JSON.stringify(timePickObj)}`);
+    if(fieldsArr) {
+        fieldsArr.forEach( (fieldItem: any) => {
+            if(fieldItem.key === 'Reminder Name') {
+                reminderObj.title = fieldItem.value;
+            }
+            if(fieldItem.key === 'Reminder Description') {
+                reminderObj.description = fieldItem.value;
+            }
+            if(fieldItem.key === 'Reminder Date') {
+                reminderObj.dueDate = fieldItem.value;
+            }
+            if(fieldItem.key ==='recurring') {
+                reminderObj.recurring = fieldItem.value;
+            }
+            if(fieldItem.key === 'priority') {
+                reminderObj.priority = fieldItem.value;
+            }
+            if(fieldItem.key === 'tags') {
+                reminderObj.tags = fieldItem.value;
+            }
+        });
+    }
+    if(datePickObj && timePickObj) {
+        const dateStr = datePickObj.initial_date;
+        const timeStr = timePickObj.initial_time;
+        reminderObj.date = dateStr
+        reminderObj.time = timeStr;
+        reminderObj.dueDate = dateStr.replaceAll("/", "-").concat(" ").concat(timeStr).concat(":00");
+    }
+    return reminderObj;
+}
+
+async function updateReminderFormFields(botRequest: any) {
+    console.log(`inside updateReminderFormFields`);
+    const {event, payload: {robotJid, toJid, accountId, messageId , fieldEditItem, actionItem } } = botRequest;
+    const botReqPayloadObjObj  = botRequest.payload.object;
+    const datepicker_item = (botReqPayloadObjObj ? botReqPayloadObjObj.datepicker_item : undefined);
+    const timepicker_item = (botReqPayloadObjObj ? botReqPayloadObjObj.timepicker_item : undefined);
+    const bot_msg_id = (botReqPayloadObjObj ? botReqPayloadObjObj.bot_msg_id : undefined);
+    const msgId =  messageId ? messageId:bot_msg_id;
+    console.log(`msgId is ${msgId}`);
+    console.log(`eventName = ${event} , messageId = ${messageId}, actionItem = ${JSON.stringify(actionItem)}, fieldEditItem = ${JSON.stringify(fieldEditItem)} ,bot_msg_id = ${bot_msg_id} , datepicker_item = ${JSON.stringify(datepicker_item)}, timepicker_item = ${JSON.stringify(timepicker_item)} `);
+    let reminderObj = getReminderObjFromContentJson(botReqPayloadObjObj? botRequest.payload.object.original: botRequest.payload.original);
+    if(fieldEditItem) {
+        console.log(`fieldEditItem = ${JSON.stringify(fieldEditItem)}`);
+        if(fieldEditItem.key === 'Reminder Name') {
+            reminderObj.title = fieldEditItem.newValue;
+        }
+        if(fieldEditItem.key === 'Reminder Description') {
+            reminderObj.description = fieldEditItem.newValue;
+        }
+        if(fieldEditItem.key === 'Reminder Date') {
+            reminderObj.dueDate = fieldEditItem.newValue;
+        }
+        if(fieldEditItem.key ==='recurring') {
+            reminderObj.recurring = fieldEditItem.newValue;
+        }
+        if(fieldEditItem.key === 'priority') {
+            reminderObj.priority = fieldEditItem.newValue;
+        }
+        if(fieldEditItem.key === 'tags') {
+            reminderObj.tags = fieldEditItem.newValue;
+        }
+    }
+
+    if (datepicker_item) {
+       if(datepicker_item.action_id === "datepicker123") {
+           reminderObj.date = datepicker_item.value;
+           reminderObj.dueDate = reminderObj.date.replaceAll("/", "-").concat(" ").concat(reminderObj.time).concat(":00");
+       }
+    }
+    if (timepicker_item) {
+        if(timepicker_item.action_id === "timepicker123") {
+            reminderObj.time = timepicker_item.value;
+            reminderObj.dueDate = reminderObj.date.replaceAll("/", "-").concat(" ").concat(reminderObj.time).concat(":00");
+        }
+    }
+    console.log(`reminderObj after updateFormFields = ${JSON.stringify(reminderObj)}`);
+    let contentStrUpd = getContentStrForCreateReminderConfirmation(botRequest, reminderObj);
+    await updateChatBotImMsg(msgId, contentStrUpd);
+}
+
+
+async function updateChatBotImMsg(msgId: string, contentStr: string) {
+    console.log(`updating chatBotMsg contentStr = ${contentStr}`);
+    if (!msgId) {
+        console.log(`msgId is not found. skipping...`);
+        return;
+    }
+    const data = await (
+        await fetch(`${zoomApiHost}/v2/im/chat/messages/${msgId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${await getAccessToken()}`,
+            },
+            body: contentStr,
+        })
+    ).json();
+    console.log(data);
+}
+
+
+async function deleteChatBotImMsg(msgId: string) {
+    if (!msgId) {
+        console.log(`msgId is not found. skipping...`);
+        return;
+    }
+    console.log(`deleting chatBotMsg msgId = ${msgId}`);
+    const data = await (
+        await fetch(`${zoomApiHost}/v2/im/chat/messages/${msgId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${await getAccessToken()}`,
+            }
+        })
+    ).json();
+    console.log(data);
+}
+
+
+function getContentStrForCreateReminderConfirmation(botRequest: any, reminderObj: any) :any {
+    const {payload: {robotJid, toJid, accountId,userJid, account_id}} = botRequest;
+    const { user_jid,robot_jid, to_jid} = botRequest.payload.object ?  botRequest.payload.object : {};
+    console.log(`botRequest = ${ JSON.stringify(botRequest)}`);
+    let jsonStr = JSON.stringify({
+        robot_jid: (robotJid ? robotJid:robot_jid),
+        to_jid:  (toJid ? toJid:to_jid) ,
+        account_id: (accountId ? accountId:account_id)  ,
+        user_jid:  (userJid ? userJid:user_jid)  ,
+        content: {
+            settings: {
+                default_sidebar_color: "#0E72ED",
+                is_split_sidebar: true,
+            },
+            head: {
+                text: "Create Reminder"
+            },
+            body: [
+                {
+                    type: "section",
+                    sections: [
+                        {
+                            type: "fields",
+                            items: [
+                                {
+                                    key: "Reminder Name" ,
+                                    value: (reminderObj.title ? reminderObj.title :" "),
+                                    editable: true,
+                                    short: false,
+                                },
+                                {
+                                    key: "Reminder Description" ,
+                                    value: (reminderObj.description ? reminderObj.description :" "),
+                                    editable: true,
+                                    short: false,
+                                },
+                                {
+                                    key: "Reminder Date",
+                                    value: (reminderObj.dueDate ? reminderObj.dueDate :" "),
+                                    editable: false,
+                                    short: false,
+                                },
+                            ],
+                        },
+                        {
+                            type: "datepicker",
+                            initial_date: reminderObj.date,
+                            action_id: "datepicker123",
+                        },
+                        {
+                            type: "timepicker",
+                            initial_time: reminderObj.time,
+                            action_id: "timepicker123",
+                        }
+                    ]
+                },
+                {
+                    type: "actions",
+                    text: "Please select your action",
+                    items: [
+                        {
+                            text: "Approve",
+                            value: "approve",
+                            style: "Primary",
+                        },
+                        {
+                            text: "Discard",
+                            value: "cancel",
+                            style: "Danger",
+                        },
+                    ],
+                }
+            ],
+        },
+    });
+    return jsonStr;
+}
+
+
+
+function defaultCommand2(botRequest: any) {
     // parse cmd str and send notification.
     const {payload: {robotJid, toJid, accountId, userJid, cmd, userId}} = botRequest;
     const contentStr = JSON.stringify({
